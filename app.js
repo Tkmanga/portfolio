7e26f2f119ce48da74572423cbbca96a82e75511
tacachojose <tacachojose@gmail.com>
2021-05-12 10:46:53 -0300

    con formulario funcional

---
+++ app.js
@@ -0,0 +1,17 @@
+const $form = document.getElementById("contacto");
+const $btnMailto = document.getElementById("truco");
+
+$form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const form = new FormData(this);
+  console.log(form.get("name"));
+  $btnMailto.setAttribute(
+    "href",
+    `mailto:tacachojose@gmail.com?subject=${form.get("name")} ${form.get(
+      "email"
+    )}&body=${form.get("message")}`
+  );
+  $btnMailto.click();
+}
